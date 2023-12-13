b6398f1ce3da968925820a8b8f73aa5b47f8e5e2
akirabrand <akira@akirabrands-MacBook-Pro.local>
2023-12-13 14:02:47 -0700

    updates talkteasercomponent

--- components/Events/talkTeaser-component.tsx
+++ components/Events/talkTeaser-component.tsx
@@ -1,26 +1,29 @@
 import stylesHp from '../../styles/Homepage/Homepage.module.css' 
 import Talk, {TalkResults} from "../../types/Events/talk-type"; 
+import Speaker, {SpeakerResults} from "../../types/Events/speaker-type";
 import Link from 'next/link' 
 import Image from 'next/image' 
  
 type Props = { 
     allTalks: TalkResults; 
+    allSpeakers: SpeakerResults;
 } 
-const TalkTeaserComponent = ({allTalks}:Props) => { 
+const TalkTeaserComponent = ({allTalks, allSpeakers}:Props) => { 
     const talks = allTalks.results; 
+    const speaker = allSpeakers.results; 
      
     return( 
         <div> 
             {talks.map((talk: Talk) => ( 
                 <div key={talk.id} className={stylesHp.boxOuter}> 
                     <div className={stylesHp.box}> 
-                        {/* <Image  
+                        <Image  
                             alt='' 
-                            src={talk.image.results[0].fileUrl}
+                            src={speaker.speakerImage.results[0].fileUrl}
                             width='500'
                             height= '500' 
                             className={stylesHp.boxImage} 
-                        />  */}
+                        /> 
                         <h2>{talk.talkTitle}</h2> 
                         <p className={stylesHp.boxText}> 
                             {talk.talkDescription} 
